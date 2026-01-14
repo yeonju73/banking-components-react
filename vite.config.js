d49b3a6daf17a8ecdfd73c3cb0a6a5e0c838422b
yeonju73 <yeon703@naver.com>
2026-01-14 10:16:18 +0900

    init: project init

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// js 에서 설정파일을은 대부분 xxx.config.js로 끝남
+// 설정 파일이 없으면 vite를 사용하는 개발자가 vite 내부 코드를 직접 건드려서 변경해야함
+// 설정 파일을 통해 정해진 옵션값을 사용하면 내부적으로 해당 옵션으로 동작하도록 적용해주는 파일
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: [
+      {find: '@', replacement: '/src'}
+    ]
+  }
+})
