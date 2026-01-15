8b3f5fdb939dc872c931f775f7fdb0958a0c51c5
aransword <lupin521@gmail.com>
2026-01-15 11:42:06 +0900

    fix: build config

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { banks } from "./data/banks";
 
 const App = () => {
   const myAccount = useAccountFormState();
+  const myclasses = 'h-full'
 
   return (
     <>
-      <AccountForm accountState={myAccount} banks={banks}/>
+      <AccountForm accountState={myAccount} banks={banks} className={myclasses}/>
       <button onClick={() => {
         console.log("최종 번호:", myAccount.accountNumber);
         console.log("최종 은행:", myAccount.accountBank);
